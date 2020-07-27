0d36fe53d985ddd35fa12de88abbadcea035fd0e
Edwin Ariza <me@edwinariza.com>
2020-07-27 12:06:13 -0500

    MODFUN: Actualización de funcionalidad es para organizar el código y generar una carpeta de ejemplos.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Simplify the AWS S3 interactions.
     <?php
     #Ejemplo para subir un archivo con permisos públicos
     require(__DIR__.'/../vendor/autoload.php');
-    require_once(__DIR__. '/S3Api.php');
+    use s3_api\S3Api;
 
     $s3=new S3Api();
     $api = '[API_KEY]';
--- src/test/index.php
+++ src/test/index.php
@@ -1,16 +1,16 @@
 <?php
 #Ejemplo para subir un archivo con permisos públicos
-require(__DIR__.'/../vendor/autoload.php');
+require(__DIR__.'/../../vendor/autoload.php');
 require_once(__DIR__. '/S3Api.php');
 
 use s3_api\S3Api;
 
 $s3=new S3Api();
-$api = '---';
-$region = '---';
-$secret = '---';
+$api = '[API_KEY]';
+$region = '[REGION]';
+$secret = '[SECRET]';
 $file = 'prueba.txt';
-$bucket = '---';
+$bucket = '[BUCKET]';
 
 $s3->connect($api,$secret, $region);
 $s3->upload($bucket, $file);
