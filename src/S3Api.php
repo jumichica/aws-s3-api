1e95211b422725f6c24e61e4dcc387c6309b2d6c
Edwin Ariza <me@edwinariza.com>
2023-05-23 08:46:17 -0500

    ARCHITECT: Update with download method.

--- src/S3Api.php
+++ src/S3Api.php
@@ -11,51 +11,81 @@ use Aws\Exception\AwsException;
 use Aws\Credentials\Credentials;
 
 class S3Api{
-  /**
-   * @var Conector al S3.
-   */
-  public $connector;
-  /**
-   * @param $key KEY generado en IAM de AWS de Amazon.
-   * @param $secret SECRET generado en IAM de Amazon AWS.
-   * @param $region Region en al que se tiene el S3.
-   */
-  public function connect($key, $secret,$region){
-    $credentials=new Credentials($key, $secret);
-    $this->connector=new S3Client([
-      'version'     => 'latest',
-      'region'      => $region,
-      'credentials' => $credentials
-    ]);
-  }
-
-  /**
-   * Permite obtener el listado de Buckets disponibles en tu cuenta.
-   * @return mixed
-   */
-  public function get_buckets(){
-    return $this->connector->listBuckets();
-  }
-
-  /**
-   * @param $bucket Nombre del bucket al que se va a subir la información.
-   * @param $filename Nombre del archivo que se va a subir. - Puede ser diferente al nombre real del archivo.
-   * @param $file_path Ruta completa al archivo que se desea subir.
-   */
-  public function upload($bucket, $filename, $file_path, $acl=""){
-    try {
-      // Upload data.
-      $result = $this->connector->putObject([
-        'Bucket' => $bucket,
-        'Key'    => $filename,
-        'SourceFile' => $file_path,
-        'ACL'    => $acl
-      ]);
-
-      // Print the URL to the object.
-      echo $result['ObjectURL'] . PHP_EOL;
-    } catch (S3Exception $e) {
-      echo $e->getMessage() . PHP_EOL;
+    /**
+    * @var Conector al S3.
+    */
+    public $connector;
+    public $bucket;
+    public $region;
+    /**
+    * @param $key KEY generado en IAM de AWS de Amazon.
+    * @param $secret SECRET generado en IAM de Amazon AWS.
+    * @param $region Region en al que se tiene el S3.
+    */
+    public function connect($key, $secret,$region){
+        $credentials=new Credentials($key, $secret);
+        $this->connector=new S3Client([
+          'version'     => 'latest',
+          'region'      => $region,
+          'credentials' => $credentials
+        ]);
     }
-  }
+    /**
+    * Permite obtener el listado de Buckets disponibles en tu cuenta.
+    * @return mixed
+    */
+    public function get_buckets(){
+        return $this->connector->listBuckets();
+    }
+
+    /**
+    * @param $bucket Nombre del bucket al que se va a subir la información.
+    * @param $filename Nombre del archivo que se va a subir. - Puede ser diferente al nombre real del archivo.
+    * @param $file_path Ruta completa al archivo que se desea subir.
+    */
+    public function upload($bucket, $filename, $file_path, $acl=""){
+        try {
+            // Upload data.
+            $result = $this->connector->putObject([
+            'Bucket' => $bucket,
+            'Key'    => $filename,
+            'SourceFile' => $file_path,
+            'ACL'    => $acl
+            ]);
+
+            // Print the URL to the object.
+            //echo $result['ObjectURL'] . PHP_EOL;
+            return true;
+        } catch (S3Exception $e) {
+          echo $e->getMessage() . PHP_EOL;
+          return false;
+        }
+    }
+
+    public function download($bucket, $filename, $file_path){
+        try {
+            if(!file_exists($file_path)){
+                // DOWNLOAD DATA
+                $result = $this->connector->getObject([
+                    'Bucket' => $bucket,
+                    'Key' => $filename,
+                    'SaveAs' => $file_path
+                ]);
+            }
+            return true;
+        } catch (S3Exception $e) {
+            echo $e->getMessage() . PHP_EOL;
+            return false;
+        }
+    }
+
+    /**
+     * @param $bucket
+     * @param $source
+     * @param $dest
+     * @return void
+     */
+    public function sync($bucket, $source, $dest){
+       // TODO: SYNC THE BUCKET.
+    }
 }
